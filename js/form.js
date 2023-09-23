20b84933c6f3292c98b4f71116c10199c09a4ac0
GustavoSachetto <g.sachettocruz@gmail.com>
2023-09-22 21:20:52 -0300

    Add

--- js/form.js
+++ js/form.js
@@ -1,4 +1,5 @@
 const btn = document.querySelector('#btn-mostra-senha');
+
 btn.addEventListener('click', function () {
     const input = document.querySelector('#password');
     const img = document.querySelector('#img-olho');
@@ -11,36 +12,68 @@ btn.addEventListener('click', function () {
     }
 });
 
-function inputError(input, span) {
+// function validandoInput(input, span) {
 
-    function validandoEmail(email) {
-        var patern = /\S+@\S+\.\S+/;
-        return patern.test(email);
-    }
+//     function validandoEmail(email) {
+//         var patern = /\S+@\S+\.\S+/;
+//         return patern.test(email);
+//     }
+ 
+//     function error(erro) {
+//         if (erro === true) {
+//             input.classList.add('error');
+//             span.classList.add('error');
+//         } else {
+//             input.classList.remove('error');
+//             span.classList.remove('error');
+//         }
+//     }
 
-    function error(erro) {
+//     if (input.type == 'email') {
+//         if (validandoEmail(input.value) === false) {
+//             error(true);
+//         } else {
+//             error(false);
+//         }
+//     }
+    
+//     if (input.id == 'password') {
+//         if (input.value.length < input.minLength) {
+//             error(true);
+//         } else {
+//             error(false);
+//         }
+//     }    
+// }
+
+function inputError() {
+
+    var erro = false;
+
+    inputError(erro);
+
+    function validateInput(erro) {
+
+        function validateEmail(email) {
+            var patern = /\S+@\S+\.\S+/;
+            return patern.test(email);
+        }
+    
+        if (input.type == 'email') {
+            if (validateEmail(input.value) === false) {
+                erro = true;
+            } 
+        }
+        
+        if (input.id == 'password') {
+            if (input.value.length < input.minLength) {
+                erro = true;
+            } 
+        }
+
         if (erro === true) {
             input.classList.add('error');
             span.classList.add('error');
-        } else {
-            input.classList.remove('error');
-            span.classList.remove('error');
-        }
+        } 
     }
-
-    if (input.type == 'email') {
-        if (validandoEmail(input.value) === false) {
-            error(true);
-        } else {
-            error(false);
-        }
-    }
-    
-    if (input.id == 'password') {
-        if (input.value.length < input.minLength) {
-            error(true);
-        } else {
-            error(false);
-        }
-    }    
 }
