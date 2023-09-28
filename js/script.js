b1d2a05fa1d5d433e5e40ad208982483dfddfad5
GustavoSachetto <g.sachettocruz@gmail.com>
2023-09-28 17:23:40 -0300

    Add

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ const header = document.querySelector('header');
 document.addEventListener('scroll',()=>{
     header.classList.toggle('scroll', scrollY > 20);
 });
+
+const menuResponsivo = document.querySelector('#menuResponsivo');
+const nav = document.querySelector('nav');
+
+menuResponsivo.addEventListener('click', () => {
+    menuResponsivo.classList.toggle('ativo');
+    nav.classList.toggle('ativo');
+});
